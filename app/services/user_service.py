bff73d0749e264a05bc8f443bb69cbf6439e385f
Rais Uddin <raisdhaka@gmail.m>
2025-05-13 17:12:12 +0600

    user_id

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -10,3 +10,7 @@ class UserService:
         db.commit()
         db.refresh(db_user)
         return db_user
+    
+    @staticmethod
+    def get_user_by_id(db: Session, user_id: int) -> User:
+        return db.query(User).filter(User.id == user_id).first()
